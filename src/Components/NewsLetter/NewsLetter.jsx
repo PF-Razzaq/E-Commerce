2ad4f35219d181c0b6230c047957281085ab57a1
Abdul Razzaq <abdulrazzaqchohan1@gmail.com>
2024-02-19 17:52:13 +0500

    ShopContext and Shop Pages Added

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -4,11 +4,13 @@ import "./NewsLetter.css";
 const NewsLetter = () => {
   return (
     <>
-      <div className="newsletter">Get Exclusive Offers On Your Email</div>
-      <p>Subscribe to our newletter and stay updated</p>
-      <div>
-        <input type="email" placeholder="Enter Your Email" />
-        <button>Subscribe</button>
+      <div className="newsletter">
+        <h1>Get Exclusive Offers On Your Email</h1>
+        <p>Subscribe to our newletter and stay updated</p>
+        <div>
+          <input type="email" placeholder="Enter Your Email" />
+          <button>Subscribe</button>
+        </div>
       </div>
     </>
   );
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import "./css/ShopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
 
-const ShopCategory = () => {
-  return <div></div>;
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
+  return (
+    <>
+      <div className="shop-category"></div>
+    </>
+  );
 };
 
 export default ShopCategory;
